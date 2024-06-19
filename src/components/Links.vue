436fb951dca089f89584868530c4f8c0123fc0ec
Nexchard <61868296+Nexchard@users.noreply.github.com>
2024-06-19 15:00:31 +0800

    增加引入Hdd图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Archive,Atlas,Atlassian,Cloudscale,CompressArrowsAlt,Code,Ethernet,Expand,Film,GlobeAsia,Robot,Sourcetree,Sync } from "@vicons/fa"; // 注意使用正确的类别
+import { Archive,Atlas,Atlassian,Cloudscale,CompressArrowsAlt,Code,Ethernet,Expand,Film,GlobeAsia，Hdd,Robot,Sourcetree,Sync } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,6 +73,7 @@ const siteIcon = {
   Expand,
   Film,
   GlobeAsia,
+  Hdd,
   Robot,
   Sourcetree,
   Sync
