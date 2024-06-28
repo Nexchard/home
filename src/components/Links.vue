9761874b83be73fef09a13049fc9c3707021339c
Nexchard <2722561490@qq.com>
2024-06-28 13:50:17 +0800

    引入新图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Archive,Atlas,Atlassian,Cloudscale,CompressArrowsAlt,Code,Ethernet,Expand,Film,GlobeAsia,Hdd,Robot,Sourcetree,Sync } from "@vicons/fa"; // 注意使用正确的类别
+import { Archive,Atlas,Atlassian,Cloudscale,CompressArrowsAlt,Code,Ethernet,Expand,Film,GlobeAsia,Hdd,Robot,Sourcetree,Sync,Tv } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,7 +76,8 @@ const siteIcon = {
   Hdd,
   Robot,
   Sourcetree,
-  Sync
+  Sync,
+  Tv
 };
 
 // 链接跳转
